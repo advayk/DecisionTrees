e5b2411ca7403b835b8db9ca903768f813a608a2
advayk <advay.koranne@gmail.com>
2019-09-14 19:47:20 -0700

    Able to print lines on a new line, and entropy function is working

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,13 +1,27 @@
+# Advay Koranne
+# Machine Learning Project
+# September 14 2019
+
+
+import math
 
 file = list()
 def ReadFile(file):
-    with open(file) as f:
-        lines = f.readlines()
-        print(lines)
-
+    lines = open(file).read()
+    #list = []
+    #list.append(lines)
+    print(lines)
+    #for var in list:
+        #print(list.index(var), var)
 
 ReadFile("TennisDataSet.txt")
 
 
-#def entropy():
-#    entropy = -(Yes)*(log(yes)/log(2)) - (pNo * (log_2(no)/log(2))
+def entropy(Yes,No):
+    entropy = -(Yes)*(math.log(Yes)/math.log(2)) - (No) * (math.log(No)/math.log(2))
+    print (entropy)
+
+Yes = 5
+No = 10
+
+entropy(Yes,No)
