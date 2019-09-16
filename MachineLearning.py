f31acb34eba9139559e229256cfeb455d6425c4f
advayk <advay.koranne@gmail.com>
2019-09-16 13:08:38 -0700

    Update MachineLearning.py

--- MachineLearning.py
+++ MachineLearning.py
@@ -3,16 +3,36 @@
 # September 14 2019
 
 
-import math
+# DataStructure: dictionary
 
-file = list()
+# play tennis,outlook,temperature,humidity,wind ( each one of these are the keys)
+# no,sunny,hot,high,weak (one day) - one entry in the linked list [0]  contains dictioaaries where the key is the atribute
+
+
+
+import math
+myfile = open("TennisDataSet.txt","r")
+list = []
+training_set = []
+texting_set = []
+
+header = myfile.readline().strip().split(',')
+categroy = header[0] # depdent
+attributes = header[1:] # end is default # attributes( indepdent)
+
+print("---", header,"---")
+print("")
+print(attributes)
+
 def ReadFile(file):
-    lines = open(file).read()
-    #list = []
-    #list.append(lines)
-    print(lines)
-    #for var in list:
-        #print(list.index(var), var)
+    for line in open(file):
+        list.append(line)
+        line = line.strip()
+        items = line.split(',')
+        print(line)
+
+    print(list[0])
+
 
 ReadFile("TennisDataSet.txt")
 
